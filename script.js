d3555ffdb9f33346c89bc566706275e7ed14cf4f
Danil <DMhayrulin@gmail.com>
2020-09-29 22:54:58 +0500

    practica-1

--- script.js
+++ script.js
@@ -21,3 +21,19 @@ console.log(answers);
 const isChecking = false,
       isClose = false; 
 console.log(isChecking || isClose);*/
+const numberOfFilms = prompt('How many films have you watched?');//№1
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};//№2
+const a = prompt('One of the last watched films?', ''),
+      b = prompt('How much would you rate it?', ''),
+      c = prompt('One of the last watched films', ''),
+      d = prompt('How much would you rate it?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
