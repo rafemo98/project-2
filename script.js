6bb45b31a32643092eae8595a4e00c723d039462
Danil <DMhairulin@gmail.com>
2020-09-29 21:24:32 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+/*const da = 'eat';
+let ne = 8;
+console.log(ne);
+const num = 0.6;
+const str = 'cat';
+const bool = true;
+const obj = {
+    name: 'Danil',
+    lastName: 'Khairulun',
+    age: 15
+};
+console.log(obj.name);
+const arr = ['danil', 'dad', 'mom', 8, 9.93, true, false, {}]
+console.log(arr[7]);
+const result = confirm('You have cat or dog?');
+const answers = [];
+answers[0] = prompt('How old are you?');
+answers[1] = prompt(`What's you name?`);
+answers[2] = confirm('You love mom and dad' );
+console.log(answers);
+const isChecking = false,
+      isClose = false; 
+console.log(isChecking || isClose);*/
